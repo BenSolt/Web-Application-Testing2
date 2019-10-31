b7d6147cf92e646588c3229666b83a5e360b38fc
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-10-31 14:52:56 -0400

    starting project

---
+++ my-app/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Dashboard = props => {
+
+    return(
+<div>
+    <button className='btn' onClick={}>Strikes</button>
+    <button>Balls</button>
+    <button>Foul</button>
+    <button>Hit</button>
+</div>
+    )
+}
